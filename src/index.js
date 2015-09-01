40466bcc0d4ad86576454b8e8003f89ce311e2a0
tquetano-r7 <Tony_Quetano@rapid7.com>
2015-09-01 15:20:19 -0400

    Add checker to verify input is an object before running

--- src/index.js
+++ src/index.js
@@ -18,23 +18,25 @@ function isValueSupported(prop, value, defaultValue) {
 }
 
 function applyPrefixes(obj) {
-    Object.keys(obj).forEach(function(key) {
-        if (typeof obj[key] === "object" && !!obj[key]) {
-            obj[key] = applyPrefixes(obj[key]);
-        } else if (properties.indexOf(key) !== -1 && !isPropertySupported(key)) {
-            let value = obj[key],
-                prefixedKey = prefix.js + key.charAt(0).toUpperCase() + key.slice(1);
+    if (typeof obj === "object" && !!obj) {
+        Object.keys(obj).forEach(function (key) {
+            if (typeof obj[key] === "object" && !!obj[key]) {
+                obj[key] = applyPrefixes(obj[key]);
+            } else if (properties.indexOf(key) !== -1 && !isPropertySupported(key)) {
+                let value = obj[key],
+                    prefixedKey = prefix.js + key.charAt(0).toUpperCase() + key.slice(1);
 
-            if (key === "transition") {
-                value = value.replace(/transform/g, prefix.css + "transform");
+                if (key === "transition") {
+                    value = value.replace(/transform/g, prefix.css + "transform");
+                }
+
+                delete obj[key];
+                obj[prefixedKey] = value;
+            } else if (key === "display" && obj[key] === "flex" && !isValueSupported("display", "flex", "block")) {
+                obj[key] = (prefix === "ms" ? "-ms-flexbox" : prefix.css + "flex");
             }
-
-            delete obj[key];
-            obj[prefixedKey] = value;
-        } else if(key === "display" && obj[key] === "flex" && !isValueSupported("display", "flex", "block")) {
-            obj[key] = (prefix === "ms" ? "-ms-flexbox" : prefix.css + "flex");
-        }
-    });
+        });
+    }
 
     return obj;
 }
