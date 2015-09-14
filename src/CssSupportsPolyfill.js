53e641d679c6aec99b5457b8865a03e38762daf2
tquetano-r7 <Tony_Quetano@rapid7.com>
2015-09-14 10:07:06 -0400

    More proper supports checker

---
+++ src/CssSupportsPolyfill.js
@@ -0,0 +1,35 @@
+var el = document.createElement("div"),
+    camelRe = /-([a-z]|[0-9])/ig,
+    support,
+    camel;
+
+export default function(prop, value){
+    // If no value is supplied, use "inherit"
+    value = arguments.length === 2 ? value : "inherit";
+
+    // Try the native standard method first
+    if ("CSS" in window && "supports" in window.CSS) {
+        return window.CSS.supports(prop, value);
+    }
+
+    // Check Opera's native method
+    if ("supportsCSS" in window) {
+        return window.supportsCSS(prop, value);
+    }
+
+    // Convert to camel-case for DOM interactions
+    camel = prop.replace(camelRe, function(all, letter) {
+        return (letter + "").toUpperCase();
+    });
+
+    // Check if the property is supported
+    support = (camel in el.style);
+
+    // Assign the property and value to invoke
+    // the CSS interpreter
+    el.style.cssText = prop + ":" + value;
+
+    // Ensure both the property and value are
+    // supported and return
+    return support && (el.style[camel] !== "");
+}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import prefix from "./prefix";
 import properties from "./properties";
 import animatableValues from "./animatableValues";
-
-var div = document.createElement("div");
+import supports from "./CssSupportsPolyfill";
 
 function camelToKebab(str) {
     return str.replace(/\W+/g, "-").replace(/([a-z\d])([A-Z])/g, "$1-$2").toLowerCase();
-}
-
-function isPropertySupported(prop) {
-    return typeof div.style[prop] === "string";
-}
-
-function isValueSupported(prop, value, defaultValue) {
-    div.style[prop] = defaultValue;
-
-    try {
-        div.style[prop] = value;
-    } catch(e) {}
-
-    return div.style[prop] === value;
 }
 
 function applyPrefixes(obj) {
@@ -29,7 +14,7 @@ function applyPrefixes(obj) {
 
             if (typeof obj[key] === "object" && !!obj[key]) {
                 obj[key] = applyPrefixes(obj[key]);
-            } else if (properties.indexOf(key) !== -1 && !isPropertySupported(key)) {
+            } else if (properties.indexOf(key) !== -1 && !supports(key)) {
                 let value = obj[key];
 
                 realKey = prefix.js + key.charAt(0).toUpperCase() + key.slice(1);
@@ -38,13 +23,13 @@ function applyPrefixes(obj) {
                 obj[realKey] = value;
             }
 
-            if (key === "display" && obj[key] === "flex" && !isValueSupported("display", "flex", "block")) {
-                obj[key] = (prefix === "ms" ? "-ms-flexbox" : prefix.css + "flex");
+            if (realKey === "display" && obj[realKey] === "flex" && !supports("display", "flex")) {
+                obj[realKey] = (prefix === "ms" ? "-ms-flexbox" : prefix.css + "flex");
             }
 
             if (key === "transition") {
                 animatableValues.forEach(function(animatableValue) {
-                    if (!isPropertySupported(animatableValue)) {
+                    if (!supports(animatableValue)) {
                         var kebabValue = camelToKebab(animatableValue),
                             re = new RegExp(kebabValue, "g");
 
